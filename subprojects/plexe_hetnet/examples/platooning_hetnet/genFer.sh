580f0d81a018c4ba36dbeae7fc2750aa97016561
alexmarchi28 <alex.marchi2000@gmail.com>
2026-03-19 16:51:35 +0100

    change run number in script

--- subprojects/plexe_hetnet/examples/platooning_hetnet/genFer.sh
+++ subprojects/plexe_hetnet/examples/platooning_hetnet/genFer.sh
@@ -1,4 +1,4 @@
-./run -u Cmdenv -c InterferenceScenarioNoGui -r 0
+./run -u Cmdenv -c InterferenceScenarioNoGui -r 4
 cd analysis
 make InterferenceScenarioFER.Rdata InterferenceScenarioEvents.Rdata InterferenceScenarioDynamics.Rdata InterferenceScenarioHandovers.Rdata
 cd dynamics-plots
